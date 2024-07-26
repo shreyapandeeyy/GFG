8162fdf4b211dda4e0a651e969cb784d446daa0a
Shreya Pandey <130491256+shreyapandeeyy@users.noreply.github.com>
2024-07-26 21:45:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find all factorial numbers less than or equal to n/find-all-factorial-numbers-less-than-or-equal-to-n.java
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            long N = sc.nextLong();
+
+            Solution ob = new Solution();
+            ArrayList<Long> ans = ob.factorialNumbers(N);
+            for (long num : ans) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    static ArrayList<Long> factorialNumbers(long n) {
+        // code here
+        ArrayList<Long> list = new ArrayList<>();
+        long fact =1;
+        for(long i=1; i<=n; i++){
+            fact *=i;
+            if(fact <=n){
+                list.add(fact );
+            } else break;
+        }return list;
+    }
+}
