3274a4cf845e42f051f824c4c0ea2f5d09ba24cb
Shreya Pandey <130491256+shreyapandeeyy@users.noreply.github.com>
2024-07-27 18:38:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Remove loop in Linked List/remove-loop-in-linked-list.java
@@ -0,0 +1,133 @@
+//{ Driver Code Starts
+// driver code
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+}
+
+class GFG {
+    public static Node newNode(int data) {
+        Node temp = new Node();
+        temp.data = data;
+        temp.next = null;
+        return temp;
+    }
+
+    public static void makeLoop(Node head, int x) {
+        if (x == 0) return;
+        Node curr = head;
+        Node last = head;
+
+        int currentPosition = 1;
+        while (currentPosition < x) {
+            curr = curr.next;
+            currentPosition++;
+        }
+
+        while (last.next != null) last = last.next;
+        last.next = curr;
+    }
+
+    public static boolean detectLoop(Node head) {
+        Node hare = head.next;
+        Node tortoise = head;
+        while (hare != tortoise) {
+            if (hare == null || hare.next == null) return false;
+            hare = hare.next.next;
+            tortoise = tortoise.next;
+        }
+        return true;
+    }
+
+    public static int length(Node head) {
+        int ret = 0;
+        while (head != null) {
+            ret += 1;
+            head = head.next;
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+
+            String str[] = read.readLine().trim().split(" ");
+            int n = str.length;
+
+            int num = Integer.parseInt(str[0]);
+            Node head = newNode(num);
+            Node tail = head;
+
+            for (int i = 1; i < n; i++) {
+                num = Integer.parseInt(str[i]);
+                tail.next = newNode(num);
+                tail = tail.next;
+            }
+
+            int pos = Integer.parseInt(read.readLine());
+            makeLoop(head, pos);
+
+            Solution x = new Solution();
+            x.removeLoop(head);
+
+            if (detectLoop(head) || length(head) != n)
+                System.out.println("false");
+            else
+                System.out.println("true");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/*
+class Node
+{
+    int data;
+    Node next;
+}
+*/
+
+class Solution {
+    // Function to remove a loop in the linked list.
+    public static void removeLoop(Node head) {
+        // code here
+        // remove the loop without losing any nodes
+        if(head==null || head.next==null) return;
+        //detect loop
+        Node slow = head;
+        Node fast = head;
+        while(fast!= null && fast.next!= null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow==fast){ //loop detected
+                break;
+            }
+        }
+        if(fast==null || fast.next==null){ // if no loop detected
+            return;
+        }
+        //find the start of the loop
+        slow = head;
+        while(slow!= fast){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        //find the node just before the start of the loop
+        Node loopNode = slow;
+        while(fast.next != loopNode){
+            fast = fast.next;
+        }
+        //remove the loop
+        fast.next = null;
+    }
+}
