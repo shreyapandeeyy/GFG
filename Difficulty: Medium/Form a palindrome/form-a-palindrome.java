e8bd010f4e02da047594cf18973852b9d287342b
Shreya Pandey <130491256+shreyapandeeyy@users.noreply.github.com>
2024-07-27 23:08:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Form a palindrome/form-a-palindrome.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*; 
+
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0){
+            String str = read.readLine();
+
+            Solution ob = new Solution();
+            
+            System.out.println(ob.countMin(str));
+        }
+    } 
+} 
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+//User function Template for Java
+
+class Solution{
+    static int countMin(String str)
+    {
+        int n = str.length();
+        
+        int[][] dp = new int[n][n];
+        
+        // Fill the dp array using the gap strategy
+        for (int gap = 1; gap < n; gap++) {
+            for (int i = 0, j = gap; j < n; i++, j++) {
+                if (str.charAt(i) == str.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(dp[i][j - 1], dp[i + 1][j]) + 1;
+                }
+            }
+        }
+        
+        return dp[0][n - 1];
+    }
+}
