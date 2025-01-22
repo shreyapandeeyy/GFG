81ff44b91af4b0596722186c98e8ea323de02386
Shreya Pandey <130491256+shreyapandeeyy@users.noreply.github.com>
2025-01-22 11:57:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Longest substring with distinct characters/longest-substring-with-distinct-characters.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.HashSet;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String s = read.readLine();
+            Solution ob = new Solution();
+            System.out.println(ob.longestUniqueSubstr(s));
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+
+
+// User function Template for Java
+class Solution {
+    public int longestUniqueSubstr(String s) {
+        HashSet<Character> set = new HashSet<>();
+        int left = 0, maxLength = 0;
+        for (int right = 0; right < s.length(); right++) {
+            while (set.contains(s.charAt(right))) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+            set.add(s.charAt(right));
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+}
